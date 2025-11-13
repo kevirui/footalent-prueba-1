22875c00d1bf0d032ef716ee559a7c3e12fbfeb3
ecc97 <carmonaedwin1123@gmail.com>
2025-11-13 13:33:55 -0500

    feat: add function to obtain users by pagination

--- frontend/src/services/users.ts
+++ frontend/src/services/users.ts
@@ -1,15 +1,21 @@
 import { API_BASE_URL } from "../lib/constants";
-
-interface IUser {
-  id: string;
-  name: string;
-  email: string;
-  role: string;
-}
+import { IUser, UsersResponse } from "../types/users";
 
 
 export async function getUserById(id: string): Promise<IUser>{
     const response = await fetch(`${API_BASE_URL}/users/${id}`);
     const dataUser = await response.json();
     return dataUser;
+}
+
+export async function getAllUsers(page: number, limit: number): Promise<UsersResponse> {
+  // Realiza la solicitud a la API incluyendo los parámetros de paginación
+  const response = await fetch(`${API_BASE_URL}/users?page=${page}&limit=${limit}`);
+
+  if (!response.ok) {
+    throw new Error(`Error al obtener usuarios: ${response.status}`);
+  }
+
+  const data: UsersResponse = await response.json();
+  return data;
 }
